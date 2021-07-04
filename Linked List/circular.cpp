e28974fe6c5c7e0d31b1f06c54c1688fa1a63f88
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2021-07-04 23:41:07 +0530

    circular linked list intro

---
+++ Linked List/circular.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class node {
+    
+    public:
+    int data;
+    node* next;
+
+    node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtHead(node* &head, int val) {
+    node* n = new node(val);
+    
+    if(head == NULL) {
+        n->next = n;
+        head = n;
+        return;
+    }
+
+    node* temp = head;
+    while(temp->next!=head) {
+        temp = temp->next;
+    }
+
+    temp->next = n;
+    n->next = head;
+    head = n;
+}
+
+void insertAtTail(node* &head, int val) {
+    
+    if(head == NULL) {
+        insertAtHead(head, val);
+        return;
+    }
+    node* temp = head;
+    node* n = new node(val);
+    while(temp->next!=head) {
+        temp = temp->next;
+    }
+    temp->next = n;
+    n->next = head;
+}
+
+void display(node* head) {
+    node* temp = head;
+    do {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }while(temp!=head);
+
+    cout<<"head\n";
+}
+
+int main() {
+    
+    node* head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    insertAtHead(head, 1);
+    insertAtHead(head, 2);
+    insertAtHead(head, 3);
+    insertAtHead(head, 4);
+    display(head);
+    return 0;
+}
