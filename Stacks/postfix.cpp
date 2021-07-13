58d3ae7066654fb4ce66d57926023e9ccb7921bc
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2021-07-13 16:52:24 +0530

    postfix

---
+++ Stacks/postfix.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int postfix(string s) {
+        stack<int> st;
+        for(int i = 0; i<s.length(); ++i) {
+            if(s[i]>='0' && s[i]<='9') {
+                st.push(s[i]-'0');
+            }
+            else {
+                
+                int op1 = st.top();
+                st.pop();
+                int op2 = st.top();
+                st.pop();
+                
+                switch(s[i]){
+                    case '+':
+                        st.push(op1+op2);
+                        break;
+                    case '-':
+                        st.push(op1-op2);
+                        break;
+                    case '*':
+                        st.push(op1*op2);
+                        break;
+                    case '/':
+                        st.push(op1/op2);
+                        break;
+                    case '^':
+                        st.push(pow(op1,op2));
+                        break;
+                }
+            }
+        }
+        
+    return st.top();
+}
+int main() {
+    
+    cout<<postfix("234*+5+");
+    
+    return 0;
+}
