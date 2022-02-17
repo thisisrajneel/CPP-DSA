3538205db7600010ab9ce5122932eb060cc357fc
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2022-02-17 19:27:10 +0530

    swap alternates

---
+++ Arrays/swap_alternates.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void swap(int arr[], int n) {
+    for (int i = 1, j = 0; i < n; i+=2, j+=2)
+    {
+        int temp = arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    
+}
+
+int main() {
+    int arr[100],n;
+    cout<<"enter number of terms :";
+    cin>>n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    
+    swap(arr, n);
+    cout<<endl;
+    for (int j = 0; j < n; j++)
+    {
+        cout<<arr[j]<<" ";
+    }
+    
+    return 0;
+}
