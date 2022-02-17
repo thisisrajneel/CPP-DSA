054bdb0be7d5f7257abfc177ec720213867de6fe
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2022-02-17 18:13:47 +0530

    array min max

---
+++ Arrays/practice.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    // finding max and min in an array
+
+    int arr[10],n;
+    int min, max;
+
+    cout<<"enter number of terms :";
+    cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    min=arr[0];
+    max=arr[0];
+    for (int j = 0; j < n; j++)
+    {
+        if(arr[j]>max) {
+            max=arr[j];
+        }
+
+        if(arr[j]<min) {
+            min=arr[j];
+        }
+    }
+    
+    cout<<"max : "<<max<<" min : "<<min;
+    
+    return 0;
+}
