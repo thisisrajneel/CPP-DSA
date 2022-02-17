1667859ff66776f7e2765f2c5d8a02a310e36ba2
Rajneel Anuraj <rajneelanuraj2001@gmail.com>
2022-02-17 18:03:53 +0530

    fibonacci for n terms

--- practice.cpp
+++ practice.cpp
@@ -6,12 +6,14 @@ int main() {
     
     // 0 1 1 2 3 5 8 13
 
-    int n = 10;
+    int n;
+    cout<<"enter number of terms :";
+    cin>>n;
 
     int a = 0;
     int b = 1;
     cout<<a<<" "<<b<<" ";
-    for(int i = 3; i<=10 ; ++i) {
+    for(int i = 3; i<=n ; ++i) {
         int c = a + b;
         cout<<c<<" ";
         a = b;
